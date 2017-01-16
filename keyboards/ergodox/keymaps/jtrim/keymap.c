68011d8b6e07e0fe45c33ff15b9ee6e216c389d5
Jesse Trimble <jesseltrimble@gmail.com>
2017-01-16 09:36:40 -0700

    Key tweaks, add Makefile

--- keyboards/ergodox/keymaps/jtrim/keymap.c
+++ keyboards/ergodox/keymaps/jtrim/keymap.c
@@ -21,21 +21,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_GRV,  KC_1,  KC_2,   KC_3,   KC_4,   KC_5,  KC_6,
         KC_TAB,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,  TG(SYMB),
-        KC_NO,   KC_A,  KC_S,   KC_D,   KC_F,   KC_G,
+        KC_ESC,   KC_A,  KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT, KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,  LCAG_T(KC_NO),
         KC_F1,   KC_F2, KC_F3,  KC_F4,  KC_LGUI,
                                                        KC_HOME, KC_END,
                                                                 KC_PGUP,
-                                        CTL_T(KC_ESC), KC_LALT, KC_PGDN,
+                                               KC_LCTRL, KC_LALT, KC_PGDN,
         // right hand
         MO(MDIA),     KC_7, KC_8,     KC_9,    KC_0,   KC_MINUS, KC_EQUAL,
         TG(SYMB),     KC_Y, KC_U,     KC_I,    KC_O,   KC_P,     KC_BSLS,
                       KC_H, KC_J,     KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
-        ALL_T(KC_NO), KC_N, KC_M,     KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
+        KC_BSPC, KC_N, KC_M,     KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
                             MO(SYMB), KC_F5,   KC_F6,  KC_F7,    KC_F8,
-             KC_NO,    KC_NO,
+             KC_NO, KC_NO,
              KC_NO,
-             KC_BSPC, KC_ENTER, KC_SPC
+             KC_NO, KC_ENTER, KC_SPC
     ),
 
 [SYMB] = KEYMAP(
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, S(KC_3), S(KC_4), S(KC_9),    S(KC_0),    S(KC_MINUS),
        KC_TRNS, S(KC_5), S(KC_6), KC_LBRC,    KC_RBRC,    S(KC_8),     KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,
-                                                    KC_TRNS, KC_TRNS,
+                                             KC_SCROLLLOCK, KC_PAUSE,
                                                              KC_TRNS,
                                            KC_TRNS, KC_TRNS, KC_TRNS,
        // right hand
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS,
+       KC__VOLDOWN, KC__VOLUP,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
 ),
