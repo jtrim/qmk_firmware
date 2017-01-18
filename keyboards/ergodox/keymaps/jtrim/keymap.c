901097ae23f32c16ea9b30a3ca0774a886982a01
Jesse Trimble <jesseltrimble@gmail.com>
2017-01-18 11:57:19 -0700

    Put arrow keys on Layer 1

--- keyboards/ergodox/keymaps/jtrim/keymap.c
+++ keyboards/ergodox/keymaps/jtrim/keymap.c
@@ -40,11 +40,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                 KC_PGUP,
                                       TD(TD_CTRL_CMD), KC_LALT, KC_PGDN,
         // right hand
-        MO(MDIA),     KC_7, KC_8,     KC_9,    KC_0,   KC_MINUS, KC_EQUAL,
-        TG(SYMB),     KC_Y, KC_U,     KC_I,    KC_O,   KC_P,     KC_BSLS,
-                      KC_H, KC_J,     KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
-        KC_ENTER, KC_N, KC_M,     KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
-                            MO(SYMB), KC_F5,   KC_F6,  KC_F7,    KC_F8,
+        MO(MDIA),     KC_7, KC_8,     KC_9,    KC_0,    KC_MINUS, KC_EQUAL,
+        TG(SYMB),     KC_Y, KC_U,     KC_I,    KC_O,    KC_P,     KC_BSLS,
+                      KC_H, KC_J,     KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
+        KC_ENTER,     KC_N, KC_M,     KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+                        MO(SYMB),     KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT,
              KC_NO, KC_NO,
              KC_NO,
              KC_NO, KC_BSPC, KC_SPC
